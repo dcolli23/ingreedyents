ec7ab0cd48e6309d799f38e7da8403a9997fb915
Dylan Colli <dfco222@g.uky.edu>
2020-02-21 08:19:48 -0500

    Added HTTPRequest class
    I've added an external class for making HTTP requests. This
    is nice because it is a single-header dependency and works right
    out of the box. Doesn't do HTTPS transfers, but I don't think
    that is an issue for us.
    
    I got the HTTPRequest class from github.com/elnormous/HTTPRequest

--- app/main.cpp
+++ app/main.cpp
@@ -1,9 +1,25 @@
 #include "example.h"
+#include "HTTPRequest.hpp"
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main () {
   example my_example;
   my_example.print_hello();
+
+  // Attempting to make an HTTP request.
+  try
+  {
+    http::Request request("http://postman-echo.com/get?foo1=bar1&foo2=bar2");
+
+    // send a get request.
+    const http::Response getResponse = request.send("GET");
+    cout << string(getResponse.body.begin(), getResponse.body.end()) << endl;
+  }
+  catch (const std::exception& e)
+  {
+    cerr << "Request failed, error: " << e.what() << endl;
+  }
   return 0;
 }
