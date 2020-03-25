cfd49dff7c0847c2101d15e076c01f275fae8414
Dylan Colli <dfco222@g.uky.edu>
2020-03-25 17:34:01 -0400

    Changed IngredientRequest test to test fixture
    I've changed the IngredientRequest tests to use a test fixture
    since this is much easier to work with and does the class
    setup/teardown automatically for you.

--- tests/include/IngredientRequestTest.h
+++ tests/include/IngredientRequestTest.h
@@ -3,19 +3,23 @@
 
 const string UPC = "041631000564";
 
-TEST(IngredientRequestTest, InitializationTest) {
-  IngredientRequest* req = new IngredientRequest(UPC);
-  delete req;
-}
+class IngredientRequestTest : public testing::Test {
+  protected:
+    IngredientRequest* my_result;
 
-TEST(IngredientRequestTest, FormURLTest) {
-  IngredientRequest* req = new IngredientRequest(UPC);
-  req->form_URL();
-  string URL_test = req->get_URL();
+    // Set up the IngredientRequest during each test.
+    void SetUp() override {
+      my_result = new IngredientRequest(UPC);
+    }
+
+    void TearDown() override {};
+};
+
+TEST_F(IngredientRequestTest, FormURLTest) {
+  my_result->form_URL();
+  string URL_test = my_result->get_URL();
   string URL_truth = "https://api.spoonacular.com/food/products/upc/" + UPC 
     + "/information?";//apiKey=XXX";
   
   EXPECT_EQ(URL_test, URL_truth) << "URL did not form correctly!";
-
-  delete req;
 }
